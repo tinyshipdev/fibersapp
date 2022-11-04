1e98f33673d3574c2543b7d0c8afc96e6201309f
Adam Gray <adamgrayscale@gmail.com>
2022-11-04 13:19:15 -0500

    feat: creat user button

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { useSession, signIn, signOut } from "next-auth/react"
-import {UserCircleIcon} from "@heroicons/react/24/outline";
-import Image from "next/image";
+import { useSession, signIn } from "next-auth/react"
+import UserButton from "./UserButton";
 
 const LoginButton: React.FC = () => {
   const { data: session } = useSession();
 
   if (session) {
-    return (
-      <>
-        <button onClick={() => signOut()}>
-          {session?.user?.image && session?.user?.name ? (
-            <Image src={session?.user?.image} alt={session?.user?.name} width={20} height={20} className={'rounded-full'}/>
-          ) : (
-            <UserCircleIcon className={'w-4 w-4 text-slate-500'}/>
-          )}
-        </button>
-      </>
-    )
+    return <UserButton/>
   }
+
   return (
     <>
       <button onClick={() => signIn()}>Sign in</button>
---
+++ components/UserButton.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {signOut, useSession} from "next-auth/react";
+import Image from "next/image";
+import {UserCircleIcon} from "@heroicons/react/24/outline";
+
+const UserButton: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const { data: session } = useSession();
+
+  return (
+    <div className={'relative'}>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {session?.user?.image && session?.user?.name ? (
+          <Image src={session?.user?.image} alt={session?.user?.name} width={20} height={20} className={'rounded-full'}/>
+        ) : (
+          <UserCircleIcon className={'w-4 w-4 text-slate-500'}/>
+        )}
+      </button>
+      {isOpen && (
+        <div className={'absolute w-40 top-10 border bg-white border-slate-400 px-3 py-1 right-0 text-left drop-shadow rounded text-slate-600'}>
+        <button
+          className={'w-full text-left'}
+          onClick={() => signOut()}
+        >Sign out</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserButton;
