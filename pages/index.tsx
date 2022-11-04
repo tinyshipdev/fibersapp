0a4dcd7a9f1fe2807716d8796cd3cd4abd49977c
Adam Gray <adamgrayscale@gmail.com>
2022-11-04 12:28:16 -0500

    feat: add loading spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import {useSession} from "next-auth/react";
 import Homepage from "../components/Homepage";
 import AppRoot from "../components/AppRoot";
+import {ArrowPathIcon} from "@heroicons/react/24/outline";
 
 
 function App() {
   const { data: session, status } = useSession();
 
   if(status === 'loading') {
-    return <div>Loading...</div>
+    return (
+      <div>
+        <div className="fixed inset-0 bg-gray-200 bg-opacity-75 transition-opacity"></div>
+        <div className={'flex justify-center items-center h-screen'}>
+          <ArrowPathIcon className={'w-10 h-10 animate-spin text-slate-700'}/>
+        </div>
+      </div>
+    )
   }
 
   if(session) {
