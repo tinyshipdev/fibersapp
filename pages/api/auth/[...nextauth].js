7bc044569ec8866edebdfd4c012528ec595223a7
Adam Gray <adamgrayscale@gmail.com>
2022-11-04 09:56:38 -0500

    feat: custom log in page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,8 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
+    pages: {
+        signIn: "/auth/signin",
+    }
 }
 export default NextAuth(authOptions)
---
+++ pages/auth/signin.js
@@ -0,0 +1,33 @@
+import {getCsrfToken, getProviders, getSession, signIn} from "next-auth/react"
+
+export default function SignIn({providers}) {
+  return (
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const session = await getSession({ req });
+
+  if (session) {
+    return {
+      redirect: { destination: "/" },
+    };
+  }
+
+  return {
+    props: {
+      providers: await getProviders(),
+      csrfToken: await getCsrfToken(context)
+    },
+  }
+}
