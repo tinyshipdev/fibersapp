f2ea97f1684c52c53be7125644e7607a08613727
Adam Gray <adamgrayscale@gmail.com>
2022-11-06 17:34:44 -0600

    refactor: mobile responsive sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -9,7 +9,7 @@ const GoogleButton = ({ id }) => {
   return (
     <button
       onClick={() => signIn(id)}
-      className={'flex items-center border border-slate-300 px-4 py-3'}
+      className={'flex items-center border border-slate-300 px-4 py-3 hover:bg-slate-200 transition'}
     >
       <img className={'w-6 mr-4'} alt="Google login" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
       <span>Sign in with Google</span>
@@ -36,8 +36,8 @@ export default function SignIn({providers}) {
           </div>
         </header>
 
-        <div className={'flex justify-center items-center py-40'}>
-          <div className="container w-2/5 mx-auto text-center py-20 bg-slate-100 rounded-lg">
+        <div className={'flex justify-center items-center py-40 px-4'}>
+          <div className="container md:w-1/2 lg:w-1/3 mx-auto text-center py-10 bg-slate-100 rounded-lg">
             <div className="flex justify-center mb-10">
               <Image src={'/logo-full-black.svg'} alt={'Fibers'} width={200} height={100}/>
             </div>
