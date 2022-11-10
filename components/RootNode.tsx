1e8935c477d05b1b94341856dee940d7a69a50f6
Adam Gray <adamgrayscale@gmail.com>
2022-11-09 19:26:35 -0600

    fix: remove comments

--- components/RootNode.tsx
+++ components/RootNode.tsx
@@ -55,70 +55,6 @@ const DEFAULT_NODES: NodesInterface = {
   },
 }
 
-// function findLastChild(nodes: NodesInterface, id: string): string {
-//   if(nodes[id].children.length === 0) {
-//     return id;
-//   }
-//
-//   return findLastChild(nodes, nodes[id]?.children[nodes[id]?.children.length - 1]);
-// }
-
-// function findNearestParentSibling(
-//   nodes: NodesInterface,
-//   id: string
-// ): string {
-//   // keep searching up until the parent has a sibling
-//   const parent = nodes[id].parent;
-//   const grandparent = nodes[parent].parent;
-//
-//   if(parent === 'root') {
-//     return id;
-//   }
-//
-//   const parentIndex = nodes[grandparent]?.children?.indexOf(parent);
-//   const sibling = nodes[grandparent]?.children[parentIndex + 1];
-//
-//   if(sibling) {
-//     return sibling;
-//   }
-//
-//   return findNearestParentSibling(nodes, parent);
-// }
-
-// function setCaretPosition(id: string, pos: number) {
-//   const el: any = document.getElementById(id);
-//
-//   if(!el) {
-//     return;
-//   }
-//
-//   if(el.createTextRange) {
-//     const range = el.createTextRange();
-//     range.move('character', pos);
-//     range.select();
-//     return;
-//   }
-//
-//   if(el.selectionStart) {
-//     el.focus();
-//     el.setSelectionRange(pos, pos);
-//     return;
-//   }
-//
-//   el.focus();
-// }
-
-// function refocusInput(id: string, pos: number) {
-//   setTimeout( () => {
-//     const element = document.getElementById(id);
-//
-//     if(element) {
-//       element.focus();
-//       setCaretPosition(id, pos)
-//     }
-//   }, 10)
-// }
-
 async function fetchNodesFromRemote() {
   const data = await fetch(`/api/nodes`, {
     method: 'GET',
@@ -129,18 +65,7 @@ async function fetchNodesFromRemote() {
 
 function getDefaultNodes() {
   return DEFAULT_NODES;
-  // if(window.localStorage.getItem('nodes')) {
-  //   return JSON.parse(window.localStorage.getItem('nodes') || '')?.nodes;
-  // } else {
-  //   return DEFAULT_NODES;
-  // }
 }
-
-// function saveState(nodes: NodesInterface) {
-//   window.localStorage.setItem('nodes', JSON.stringify({
-//     nodes
-//   }));
-// }
 
 async function persistState(nodes: NodesInterface) {
   const data = await fetch('/api/nodes', {
@@ -202,20 +127,6 @@ const RootNode: React.FC = () => {
     setIsSaved(false);
   }, [nodes]);
 
-  // /**
-  //  * I'm not sure how this works, but somehow it's working perfectly
-  //  * it saves every second, but only if you're not editing.
-  //  */
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     saveState(nodes);
-  //   }, 1000);
-  //
-  //   return () => {
-  //     clearTimeout(timer);
-  //   }
-  // }, [nodes]);
-
   useEffect(() => {
     const timer = setTimeout(async () => {
       await persistState(nodes);
@@ -231,39 +142,6 @@ const RootNode: React.FC = () => {
     setHistory([...history.slice(-1000), ...items]);
   }
 
-  // function addNode(id: string, caretOffset: number) {
-  //   let parentId = nodes[id].parent;
-  //   const n = { ...nodes };
-  //   const nodeId = nanoid();
-  //
-  //   // if the cursor is at the start of the sentence, add a node BEFORE the current one
-  //   if(caretOffset === 0 && n[id].value.length > 0) {
-  //     n[nodeId] = { value: '', isExpanded: true, children: [], parent: parentId };
-  //     let index = nodes[parentId]?.children.indexOf(id);
-  //     nodes[parentId]?.children.splice(index, 0, nodeId);
-  //   } else {
-  //     /**
-  //      * when we add a node below, we might be halfway through a word
-  //      * when we hit enter, we want to split the word and create a new node with the second
-  //      * half of that word.
-  //      */
-  //     let firstHalf = n[id].value.slice(0, caretOffset);
-  //     let secondHalf = n[id].value.slice(caretOffset);
-  //
-  //     n[id].value = firstHalf;
-  //     n[nodeId] = { value: secondHalf, isExpanded: true, children: [], parent: parentId };
-  //
-  //     let index = nodes[parentId]?.children.indexOf(id);
-  //     nodes[parentId]?.children.splice(index + 1, 0, nodeId);
-  //   }
-  //
-  //   updateHistory([
-  //     { type: HistoryType.ADD_NODE, data: { currentNode: nodeId, previousNode: id, parentNode: parentId }}
-  //   ]);
-  //   setNodes(n);
-  //   refocusInput(nodeId, 0);
-  // }
-
   function addNodeAsChild(id: string) {
     const n = { ...nodes };
     const nodeId = nanoid();
@@ -278,229 +156,6 @@ const RootNode: React.FC = () => {
     setNodes(n);
     refocusInput(nodeId, 0);
   }
-
-  // function indentRight(id: string, offset: number) {
-  //   const n = { ...nodes };
-  //
-  //   let parentId = nodes[id].parent;
-  //   let subNodes = nodes[parentId]?.children;
-  //
-  //   let index = subNodes.indexOf(id);
-  //   let previousKey = subNodes[index - 1];
-  //
-  //   if(!previousKey) {
-  //     return;
-  //   }
-  //
-  //   // if the new parent is collapsed, expand it first, so we can see where this node went
-  //   if(!n[previousKey].isExpanded) {
-  //     n[previousKey].isExpanded = true;
-  //   }
-  //
-  //   n[previousKey]?.children.push(id);
-  //   subNodes.splice(index, 1);
-  //
-  //   n[id].parent = previousKey;
-  //
-  //   updateHistory([{ type: HistoryType.INDENT_RIGHT, data: { id, offset }}]);
-  //   setNodes(n);
-  //   refocusInput(id, offset);
-  // }
-
-
-  // function indentLeft(id: string, offset: number) {
-  //   const n = { ...nodes };
-  //
-  //   // find parent
-  //   // find grandparent
-  //   let parent = n[id].parent;
-  //   let grandparent = n[parent].parent;
-  //
-  //   if(!grandparent) {
-  //     return;
-  //   }
-  //
-  //   // find index of parent in grandparent
-  //   let indexOfParent = n[grandparent]?.children.indexOf(parent);
-  //
-  //   // insert id after index of parent in grandparent
-  //   n[grandparent]?.children.splice(indexOfParent + 1, 0, id);
-  //
-  //   // remove id as child of parent
-  //   let indexOfIdInParent = n[parent]?.children.indexOf(id);
-  //   n[parent]?.children.splice(indexOfIdInParent, 1);
-  //
-  //   // update parent of id to be grandparent
-  //   n[id].parent = grandparent;
-  //
-  //   updateHistory([{ type: HistoryType.INDENT_LEFT, data: { id, offset }}]);
-  //   setNodes(n);
-  //   refocusInput(id, offset);
-  // }
-
-  // TODO: refactor this to use DOM nodes instead of our data structure
-  // function findPreviousVisibleNode(id: string, offset: number, parentId: string): [id: string, offset: number ]| null {
-  //   if(offset !== 0) {
-  //     return null;
-  //   }
-  //
-  //   const parent = parentId;
-  //   const previousSiblingIndex = nodes[parent]?.children?.indexOf(id) - 1;
-  //   const previousSibling = nodes[parent]?.children[previousSiblingIndex];
-  //
-  //   // if there is no previous sibling, go to parent
-  //   if(!previousSibling) {
-  //     return [parent, nodes[parent].value.length];
-  //   }
-  //
-  //   // this is likely a shared node, so we can't move to it until we refactor this function to use DOM nodes
-  //   if(!nodes[previousSibling]) {
-  //     return null;
-  //   }
-  //
-  //   // if previous sibling is collapsed, go to previous sibling
-  //   if(!nodes[previousSibling].isExpanded) {
-  //     return [previousSibling, nodes[previousSibling].value.length];
-  //   }
-  //
-  //   // if current has previous sibling and previous sibling has children, find the very last child recursively
-  //   if(previousSibling && nodes[previousSibling].children.length > 0) {
-  //     const lastChild = findLastChild(nodes, previousSibling);
-  //     return [lastChild, nodes[lastChild].value.length];
-  //   }
-  //
-  //   // if current has previous sibling, but previous sibling has no children, move to previous sibling
-  //   if(previousSibling && nodes[previousSibling].children.length === 0) {
-  //     return [previousSibling, nodes[previousSibling].value.length];
-  //   }
-  //
-  //   return null;
-  // }
-
-  // function findNextVisibleNode(id: string, offset: number): [id: string, offset: number] | null {
-  //   if(offset !== nodes[id].value.length) {
-  //     return null;
-  //   }
-  //
-  //   const n = { ...nodes };
-  //   const parent = n[id].parent;
-  //   const currentIndex = n[parent]?.children.indexOf(id);
-  //   const sibling = n[parent]?.children[currentIndex + 1];
-  //
-  //   // if the current item is collapsed, and has a next sibling to the next sibling
-  //   if(!n[id].isExpanded && sibling) {
-  //     return [sibling, 0];
-  //   }
-  //
-  //   // if current item is collapsed, but doesn't have next sibling, go to nearest parent sibling
-  //   if(!n[id].isExpanded && !sibling) {
-  //     const nearestParentSibling = findNearestParentSibling(n, id);
-  //     return [nearestParentSibling, 0];
-  //   }
-  //
-  //   // if the current node has children, move to first child
-  //   if(n[id].children.length > 0) {
-  //     return [n[id]?.children[0], 0];
-  //   }
-  //
-  //   // if the current node does not have children, but has a sibling, move to sibling
-  //   if(n[id].children.length === 0 && sibling) {
-  //     return [sibling, 0];
-  //   }
-  //
-  //   // if the current node does not have children, and does not have a sibling
-  //   // find the nearest parent with a sibling
-  //   const nearestParentSibling = findNearestParentSibling(n, id);
-  //   // the finalCaretPosition is so if we're on the last element the caret stays at the end
-  //   let finalCaretPosition = 0;
-  //
-  //   if(id === nearestParentSibling) {
-  //     finalCaretPosition = n[nearestParentSibling].value.length;
-  //   }
-  //
-  //   return [nearestParentSibling, finalCaretPosition];
-  // }
-
-  // function handleDelete(id: string, startOffset: number, endOffset: number) {
-  //   if(!nodes[id].isExpanded) {
-  //
-  //     if(nodes[id].value.length === 0) {
-  //       // if you attempt to delete a collapsed node that's empty but has children,
-  //       // expand the node to explain why you can't delete this node
-  //       handleExpand(nodes, id);
-  //     }
-  //     return;
-  //   }
-  //
-  //   if(nodes[id].children.length > 0) {
-  //     return;
-  //   }
-  //
-  //   if(startOffset !== 0) {
-  //     return;
-  //   }
-  //
-  //   if(startOffset === 0 && endOffset === nodes[id].value.length && nodes[id].value.length > 0) {
-  //     return;
-  //   }
-  //
-  //   const n = { ...nodes };
-  //
-  //   // remove node as child of parent
-  //   const parent = n[id].parent;
-  //
-  //   // if the parent is root, and id is first child of root
-  //
-  //   if(parent === 'root' && id === nodes[parent].children[0]) {
-  //     return;
-  //   }
-  //
-  //   const indexOfCurrent = n[parent].children.indexOf(id);
-  //
-  //   updateHistory([{
-  //     type: HistoryType.DELETE_NODE,
-  //     data: {
-  //       id,
-  //       node: {...n[id]},
-  //       indexOfNodeInParent: indexOfCurrent,
-  //     }
-  //   }]);
-  //
-  //   const moveTo = findPreviousVisibleNode(id, 0);
-  //   n[parent].children.splice(indexOfCurrent, 1);
-  //
-  //
-  //   if(startOffset === 0 && endOffset === 0 && nodes[id].value.length > 0) {
-  //     // append the current value to the previous node before deleting if we're not selecting anything
-  //     if(moveTo) {
-  //       n[moveTo[0]].value = n[moveTo[0]].value + n[id].value;
-  //     }
-  //   }
-  //
-  //   delete n[id];
-  //   setNodes(n);
-  //
-  //   if(moveTo) {
-  //     refocusInput(moveTo[0], moveTo[1]);
-  //   }
-  // }
-
-  // function handleChange(id: string, value: string) {
-  //   let n = {...nodes};
-  //   updateHistory([{ type: HistoryType.CHANGE_TEXT, data: { id, value: n[id].value }}]);
-  //   n[id].value = value;
-  //   setNodes(n);
-  // }
-
-  // function handleExpand(id: string) {
-  //   const n = { ...nodes };
-  //
-  //   if(!n[id].isExpanded) {
-  //     n[id].isExpanded = true;
-  //     updateHistory([{ type: HistoryType.EXPAND_NODE, data: { id }}]);
-  //     setNodes(n);
-  //   }
-  // }
 
   function handleExpand(nodes: NodesInterface, id: string) {
     const data = onExpand(nodes, id);
@@ -520,16 +175,6 @@ const RootNode: React.FC = () => {
     updateHistory([{ type: HistoryType.COLLAPSE_NODE, data: { id }}]);
     setNodes(data.nodes);
   }
-
-  // function handleCollapse(id: string) {
-  //   const n = { ...nodes };
-  //
-  //   if(n[id].isExpanded) {
-  //     n[id].isExpanded = false;
-  //     updateHistory([{ type: HistoryType.COLLAPSE_NODE, data: { id }}]);
-  //     setNodes(n);
-  //   }
-  // }
 
   function undoCollapse(id: string) {
     const n = { ...nodes };
