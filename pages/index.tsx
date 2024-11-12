2908763181211dd66ee41e89f777802b8df73005
Adam Gray <adamgrayscale@gmail.com>
2024-11-12 14:07:46 -0600

    add buy me a coffee button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import AppRoot from "../components/AppRoot";
 import Head from "next/head";
 
@@ -9,10 +9,20 @@ function App() {
         <title>fibers - follow your thoughts</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <AppRoot/>
+      <AppRoot />
+      <a
+        target={"_blank"}
+        className="fixed bottom-8 right-8"
+        href="https://buymeacoffee.com/tinyship"
+      >
+        <img
+          src="/bmc-button.png"
+          alt="Buy me a coffee"
+          className="w-40 hover:opacity-70 cursor-pointer shadow-lg"
+        />
+      </a>
     </>
   );
-
 }
 
 export default App;
